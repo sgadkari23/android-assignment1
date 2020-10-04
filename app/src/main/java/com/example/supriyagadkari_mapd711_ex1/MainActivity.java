045c542e02512e6115168d933a89169a133ca8e3
Supriya G <sgadkar1@my.centennialcollege.ca>
2020-10-03 21:54:35 -0500

    Initial commit

---
+++ app/src/main/java/com/example/supriyagadkari_mapd711_ex1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.supriyagadkari_mapd711_ex1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
